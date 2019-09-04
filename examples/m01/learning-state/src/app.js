8eaaf3a10968c492f845eb233484f4ad6cca826d
wslmacieira <wslmacieira@outlook.com>
2019-09-04 18:32:46 -0300

    PropTypes

--- examples/m01/learning-state/src/app.js
+++ examples/m01/learning-state/src/app.js
@@ -1,37 +1,12 @@
 import React, { Component } from 'react'
 
-import Timer from './timer'
+import Button from './button'
 
 class App extends Component {
-    constructor () {
-        super()
-        this.state = {
-            time: 0,
-            showTimer: true
-        }
-        console.log('constructor app')
-        
-    }
-
-    componentWillMount () {
-        console.log('componentWillMount app')    
-    }
-
-    componentDidMount () {
-        console.log('componentDidMount app')
-        
-    }
-
-
     render() {
-        console.log('render app')
-        
         return (
             <div>
-                <Timer time={this.state.time} /> 
-                <button onClick={() =>{
-                    this.setState({ time: this.state.time + 10 })
-                }}>Change props</button>
+             <Button handleClick={() => console.log('teste')}>Clique em min</Button>
             </div>
         )
     }
