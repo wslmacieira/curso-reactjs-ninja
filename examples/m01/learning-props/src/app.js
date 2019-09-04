5cb8d608c5f2db9bb14b18b4ce19805cf10489f6
wslmacieira <wslmacieira@outlook.com>
2019-09-03 23:04:15 -0300

    passando atributo pela props

---
+++ examples/m01/learning-props/src/app.js
@@ -0,0 +1,14 @@
+import React from 'react' 
+import Title from './title'
+
+const App = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <Title name='Wagner santos' />
+            </div>
+        )
+    }
+})
+
+export default App
---
+++ examples/m01/learning-props/src/title.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Title = React.createClass({
+    render: function () {
+        return (
+            <h1>Olá {this.props.name}</h1>
+        )
+    }
+})
+
+export default Title
