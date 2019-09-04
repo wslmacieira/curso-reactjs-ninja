fa69c6c141f665a86602144aa560838085ec0c2c
wslmacieira <wslmacieira@outlook.com>
2019-09-04 15:22:29 -0300

    eventos

--- examples/m01/learning-props/src/app.js
+++ examples/m01/learning-props/src/app.js
@@ -1,13 +1,13 @@
-import React, { Component} from 'react'
+import React, { Component } from 'react'
 import Square from './square'
 
 class App extends Component {
     render() {
         return (
-            <div className='container'>
-                {['blue', 'red', 'blue'].map((square, index) => (
-                <Square key={index} color={square}/>
-                ))}
+            <div className='container' onClick={(e) =>{
+                alert('clicou')
+            }}>
+                <Square />
             </div>
         )
     }
