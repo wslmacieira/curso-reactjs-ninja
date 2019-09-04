6ec02621596566e561b028ed2c9a507ece0be3f9
wslmacieira <wslmacieira@outlook.com>
2019-09-04 18:23:03 -0300

    componentDidUpdate

--- examples/m01/learning-state/src/timer.js
+++ examples/m01/learning-state/src/timer.js
@@ -26,6 +26,10 @@ class Timer extends Component {
         
     }
 
+    componentDidUpdate (prevProps, prevState) {
+        console.log('componentDidUpdate timer', this.props, prevProps)
+    }
+
     componentDidMount () {
         console.log('componentDidMount timer')
         
