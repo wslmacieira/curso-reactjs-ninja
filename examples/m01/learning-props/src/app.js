61a279e92fc0ff6fa63ce31968e4ed142ca3c591
wslmacieira <wslmacieira@outlook.com>
2019-09-04 15:11:46 -0300

    prop Key

--- examples/m01/learning-props/src/app.js
+++ examples/m01/learning-props/src/app.js
@@ -1,11 +1,13 @@
 import React, { Component} from 'react'
-import Title from './title'
+import Square from './square'
 
 class App extends Component {
     render() {
         return (
             <div className='container'>
-                <Title name='Wagner' />
+                {['blue', 'red', 'green'].map((square) => (
+                <Square key={square} color={square}/>
+                ))}
             </div>
         )
     }
---
+++ examples/m01/learning-props/src/square.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Square = ({ color }) => (
+    <div style={{
+        backgroundColor: color,
+        height: '100px',
+        width: '100px'
+    }} />
+)
+
+Square.defaultProps = {
+    color: 'red'
+}
+
+export default Square
