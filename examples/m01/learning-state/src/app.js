80d1a9b3a1b81ec26fdb4d4fe41b3827c8930baa
wslmacieira <wslmacieira@outlook.com>
2019-09-04 17:47:02 -0300

    fluxo mount / unmount

--- examples/m01/learning-state/src/app.js
+++ examples/m01/learning-state/src/app.js
@@ -1,26 +1,36 @@
 import React, { Component } from 'react'
-import Button from './button';
-import Square from './square';
+
+import Timer from './timer'
 
 class App extends Component {
     constructor () {
         super()
         this.state = {
-            color: 'green'
+            showTimer: true
         }
+        console.log('constructor')
+        
     }
 
+    componentWillMount () {
+        console.log('componentWillMount')    
+    }
+
+    componentDidMount () {
+        console.log('componentDidMount')
+        
+    }
+
+
     render() {
+        console.log('render')
+        
         return (
             <div>
-                <Square color={this.state.color} />
-                {['green', 'red', 'blue', 'yellow'].map((color) =>(
-                <Button 
-                key={color} 
-                handleClick={() => this.setState({ color })}>
-                {color}
-                </Button>
-                ))}
+                {this.state.showTimer && <Timer /> }
+                <button onClick={() =>{
+                    this.setState({ showTimer: !this.state.showTimer })
+                }}>Show / hide timer</button>
             </div>
         )
     }
