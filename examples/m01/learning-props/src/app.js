0cd1184036ca988771ec00fec31cee51f8e9ec8c
wslmacieira <wslmacieira@outlook.com>
2019-09-04 15:32:30 -0300

    prop children

--- examples/m01/learning-props/src/app.js
+++ examples/m01/learning-props/src/app.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
-import Square from './square'
+import Button from './button'
 
 class App extends Component {
     render() {
         return (
-            <div className='container' onClick={(e) =>{
-                alert('clicou')
-            }}>
-                <Square />
+            <div className='container'>
+                <Button>
+                    <span>Texto</span>
+                    outro texto
+                </Button>
             </div>
         )
     }
