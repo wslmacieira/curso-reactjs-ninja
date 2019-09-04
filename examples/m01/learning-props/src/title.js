a28da7d849cc07b45a64f38da05ee3b6b419a34c
wslmacieira <wslmacieira@outlook.com>
2019-09-04 14:38:01 -0300

    funções puras

--- examples/m01/learning-props/src/title.js
+++ examples/m01/learning-props/src/title.js
@@ -1,27 +1,40 @@
-import React from 'react';
+import React from 'react'
 
-const Title = React.createClass({
-    getDefaultProps: function () {
-        return {
-            name: 'Desconhecido',
-            // lastname: 'Sem sobrenome'
-            lastname: {
-                first: 'Sem ',
-                last: 'Sobrenome'
-            }
-        }
-    },
-    render: function () {
+const Title = ({name, lastname}) => <h1>Olá {`${name} ${lastname}`}!</h1>
+// const Title = (props) => <h1>Olá {`${props.name} ${props.lastname}`}!</h1>
+// const Title = (props) => <h1>Olá {props.name + ' ' + props.lastname}!</h1>
+// const Title = function () {
+//     return(
+//         <h1>Olá Wagner</h1>
+//     )
+// }
+Title.defaultProps = {
+    name: 'desconhecido',
+    lastname: 'Sem sobrenome'
+}
 
-        return (
-            <h1>Olá {
-                this.props.name + ' ' + this.props.lastname.first +
-                this.props.lastname.last
-                }!</h1>
-            // <h1>Olá {this.props.name || 'Desconhecido'}!</h1>
-            // <h1>Olá {this.props.name + ' ' + this.props.lastname}!</h1>
-        )
-    }
-})
+// const Title = React.createClass({
+//     getDefaultProps: function () {
+//         return {
+//             name: 'Desconhecido',
+//             // lastname: 'Sem sobrenome'
+//             lastname: {
+//                 first: 'Sem ',
+//                 last: 'Sobrenome'
+//             }
+//         }
+//     },
+//     render: function () {
+
+//         return (
+//             <h1>Olá {
+//                 this.props.name + ' ' + this.props.lastname.first +
+//                 this.props.lastname.last
+//                 }!</h1>
+//             // <h1>Olá {this.props.name || 'Desconhecido'}!</h1>
+//             // <h1>Olá {this.props.name + ' ' + this.props.lastname}!</h1>
+//         )
+//     }
+// })
 
 export default Title
