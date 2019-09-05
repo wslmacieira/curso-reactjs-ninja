b918032ae1fa0c96ecdb67b4fa66ae7ab638c5f6
wslmacieira <wslmacieira@outlook.com>
2019-09-05 12:30:14 -0300

    fazendo request com ajax

--- examples/m01/github-app/src/components/search.js
+++ examples/m01/github-app/src/components/search.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import ajax from '@fdaciuk/ajax'
 
  const Search = () => (
      <div className="search">
       <input type="search" 
-      placeholder='Digite o nome do usuário no Github'/>
+      placeholder='Digite o nome do usuário no Github'
+      onKeyUp={(e) =>{
+        const value = e.target.value
+        const keyCode = e.which || e.keyCode
+        const ENTER = 13
+        if (keyCode === ENTER) {
+          ajax().get(`https://api.github.com/users/${value}`)
+          .then((result) => {
+            console.log(result)
+            
+          })
+        }
+        console.log(keyCode)
+      }}
+      />
     </div>
  )
 
