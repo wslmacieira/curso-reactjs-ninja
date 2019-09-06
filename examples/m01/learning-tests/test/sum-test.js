59e3b6522c1c428b3acf2eaff4f3507b51b14f2d
wslmacieira <wslmacieira@outlook.com>
2019-09-05 21:59:18 -0300

    TDD

--- examples/m01/learning-tests/test/sum-test.js
+++ examples/m01/learning-tests/test/sum-test.js
@@ -1,4 +1,13 @@
 const sum =  require('./sum')
 
-console.log(sum(1,2) === 3)
-console.log(sum(1,1) === 2)
+console.assert(typeof sum === 'function', 'sum deve ser uma função')
+
+console.assert(
+    sum(1,2) === 3, 'a soma deve retornar 3'
+)
+console.assert(
+    sum(3,2) === 5, 'a soma deve retornar 5'
+)
+
+console.log('All tests passed')
+
--- examples/m01/learning-tests/test/sum.js
+++ examples/m01/learning-tests/test/sum.js
@@ -1,5 +1,4 @@
-
-function sum (x, y) {
+function sum (x,y) {
     return x + y
 }
 
