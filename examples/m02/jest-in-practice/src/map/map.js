6539137612644c2f559cb13033a16e0275a965c1
wslmacieira <wslmacieira@outlook.com>
2019-09-08 15:58:33 -0300

    TDD - metodo map

---
+++ examples/m02/jest-in-practice/src/map/map.js
@@ -0,0 +1,10 @@
+const map = (arr = [], func = (item) => item) => {
+    let newArr = []
+
+    for (let i = 0; i < arr.length; i++) 
+        newArr.push(func(arr[i], i, arr))
+    
+    return newArr
+}
+
+export default map
